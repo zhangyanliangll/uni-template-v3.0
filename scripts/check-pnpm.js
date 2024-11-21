35b856dff306da80fa46c230400ffc07960fd904
zhangyanliang <445057661@qq.com>
2024-11-21 09:28:22 +0800

    chore: 更新

--- scripts/check-pnpm.js
+++ scripts/check-pnpm.js
@@ -1,4 +1,4 @@
-if (!/pnpm\.js$/.test(process.env.npm_execpath || '')) {
+if (!/pnpm\.cjs$/.test(process.env.npm_execpath || '')) {
   console.warn(
     '\u001b[33mThis repository requires Pnpm for scripts to work properly.\u001b[39m\n',
   )
